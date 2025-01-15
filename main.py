d0dc749160b516cf8c1740b145e48e4c202ef4b1
zhsohu88 <zhsohu8869@gmail.com>
2025-01-15 09:50:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from collections import OrderedDict
 from datetime import datetime
 import config
 
+# 配置日志记录
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', handlers=[logging.FileHandler("function.log", "w", encoding="utf-8"), logging.StreamHandler()])
 
+# 解析模板文件
 def parse_template(template_file):
     template_channels = OrderedDict()
     current_category = None
 
+    # 读取模板文件
     with open(template_file, "r", encoding="utf-8") as f:
         for line in f:
             line = line.strip()
@@ -24,6 +27,7 @@ def parse_template(template_file):
 
     return template_channels
 
+# 获取频道信息
 def fetch_channels(url):
     channels = OrderedDict()
 
@@ -73,6 +77,7 @@ def fetch_channels(url):
 
     return channels
 
+# 匹配频道信息
 def match_channels(template_channels, all_channels):
     matched_channels = OrderedDict()
 
@@ -86,6 +91,7 @@ def match_channels(template_channels, all_channels):
 
     return matched_channels
 
+# 过滤来源URL
 def filter_source_urls(template_file):
     template_channels = parse_template(template_file)
     source_urls = config.source_urls
@@ -103,59 +109,54 @@ def filter_source_urls(template_file):
 
     return matched_channels, template_channels
 
+# 判断是否为IPv6
 def is_ipv6(url):
     return re.match(r'^http:\/\/\[[0-9a-fA-F:]+\]', url) is not None
 
+# 更新频道URL到M3U文件
 def updateChannelUrlsM3U(channels, template_channels):
-    written_urls = set()
-
-    current_date = datetime.now().strftime("%Y-%m-%d")
-    for group in config.announcements:
-        for announcement in group['entries']:
-            if announcement['name'] is None:
-                announcement['name'] = current_date
+    written_urls = set()  # 创建一个集合来存储已写入的URL，以避免重复
 
     with open("live.m3u", "w", encoding="utf-8") as f_m3u:
+        # 写入M3U文件头部信息，包括EPG URL列表
         f_m3u.write(f"""#EXTM3U x-tvg-url={",".join(f'"{epg_url}"' for epg_url in config.epg_urls)}\n""")
 
         with open("live.txt", "w", encoding="utf-8") as f_txt:
-            for group in config.announcements:
-                f_txt.write(f"{group['channel']},#genre#\n")
-                for announcement in group['entries']:
-                    f_m3u.write(f"""#EXTINF:-1 tvg-id="1" tvg-name="{announcement['name']}" tvg-logo="{announcement['logo']}" group-title="{group['channel']}",{announcement['name']}\n""")
-                    f_m3u.write(f"{announcement['url']}\n")
-                    f_txt.write(f"{announcement['name']},{announcement['url']}\n")
-
-            for category, channel_list in template_channels.items():
-                f_txt.write(f"{category},#genre#\n")
-                if category in channels:
-                    for channel_name in channel_list:
-                        if channel_name in channels[category]:
+            for category, channel_list in template_channels.items():  # 遍历模板中的频道分类
+                f_txt.write(f"{category},#genre#\n")  # 写入每个频道分类
+                if category in channels:  # 如果频道分类存在于获取的频道列表中
+                    for channel_name in channel_list:  # 遍历每个频道名称
+                        if channel_name in channels[category]:  # 如果频道名称存在于获取的频道分类中
+                            # 按照IP版本优先级对URL进行排序
                             sorted_urls = sorted(channels[category][channel_name], key=lambda url: not is_ipv6(url) if config.ip_version_priority == "ipv6" else is_ipv6(url))
-                            filtered_urls = []
-                            for url in sorted_urls:
+                            filtered_urls = []  # 创建一个列表来存储过滤后的URL
+                            for url in sorted_urls:  # 遍历排序后的URL
+                                # 如果URL有效且不在已写入的URL集合中，并且不包含于黑名单中
                                 if url and url not in written_urls and not any(blacklist in url for blacklist in config.url_blacklist):
-                                    filtered_urls.append(url)
-                                    written_urls.add(url)
+                                    filtered_urls.append(url)  # 添加到过滤后的URL列表中
+                                    written_urls.add(url)  # 将URL添加到已写入的URL集合中
 
-                            total_urls = len(filtered_urls)
-                            for index, url in enumerate(filtered_urls, start=1):
+                            total_urls = len(filtered_urls)  # 获取过滤后的URL总数
+                            for index, url in enumerate(filtered_urls, start=1):  # 遍历过滤后的URL，并从1开始计数
+                                # 根据IP版本生成URL后缀
                                 if is_ipv6(url):
                                     url_suffix = f"$LR•IPV6" if total_urls == 1 else f"$LR•IPV6『线路{index}』"
                                 else:
                                     url_suffix = f"$LR•IPV4" if total_urls == 1 else f"$LR•IPV4『线路{index}』"
                                 if '$' in url:
-                                    base_url = url.split('$', 1)[0]
+                                    base_url = url.split('$', 1)[0]  # 分割URL，获取基础URL
                                 else:
                                     base_url = url
 
-                                new_url = f"{base_url}{url_suffix}"
+                                new_url = f"{base_url}{url_suffix}"  # 生成新的URL
 
-                                f_m3u.write(f"#EXTINF:-1 tvg-id=\"{index}\" tvg-name=\"{channel_name}\" tvg-logo=\"https://gcore.jsdelivr.net/gh/yuanzl77/TVlogo@master/png/{channel_name}.png\" group-title=\"{category}\",{channel_name}\n")
+                                # 写入M3U文件中的频道信息
+                                f_m3u.write(f"#EXTINF:-1 tvg-id=\"{index}\" tvg-name=\"{channel_name}\" tvg-logo=\"https://gcore.jsdelivr.net/gh/yuanzl77/TVlogo@master/png/{channel_name}.png\" group-title=\"{category}\",{channel_name}\n""")
                                 f_m3u.write(new_url + "\n")
+                                # 写入TXT文件中的频道信息
                                 f_txt.write(f"{channel_name},{new_url}\n")
 
-            f_txt.write("\n")
+            f_txt.write("\n")  # 添加空行以分隔不同部分
 
 if __name__ == "__main__":
     template_file = "demo.txt"
